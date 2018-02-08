47077583a443ed325e08ff5e5241ed364dffcf12
Luke Hammer <textchimp@gmail.com>
2018-02-08 22:53:25 +1100

    output whole data object neatly in example script using readline

--- examples/simple.js
+++ examples/simple.js
@@ -1,9 +1,11 @@
 var daydream = require('../daydream-node')();
+var readline = require('readline');
 
 daydream.onStateChange(function(data){
-    if(data.isClickDown){
-        console.log('clicking down');
-    } else {
-        console.log('not cliking down');
-    }
-})
+  // output whole data object using JSON.stringify and readline library
+  // (i.e. overwrite output instead of appending)
+  readline.clearScreenDown(process.stdout);
+  process.stdout.write(JSON.stringify(data, null, 2));
+  readline.moveCursor(process.stdout, null, -19);
+  readline.cursorTo(process.stdout, 0);
+});
